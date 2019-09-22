74305ab60a19dbe0385f7ade898ac8f8954466ad
Leonardo Jorge Tecco <teccoleo@hotmail.com>
2019-09-21 21:23:06 -0300

    Create server

---
+++ server/index.js
@@ -0,0 +1,12 @@
+// Importação do socket.io
+const socketIO = require('socket.io')
+
+// Inicia servidor socket.io
+socketIO(3000)
+
+console.log(`
+========================
+SERVIDOR INICIADO
+PORTA: 3000
+========================
+`)
